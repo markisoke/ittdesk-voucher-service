e841f3ac5d90872f3a27f48cc71c825b25bae55f
Mark Remijn <markremijn@live.nl>
2020-03-06 16:04:13 +0100

    Update uvs_config.php

--- uvs_config.php
+++ uvs_config.php
@@ -36,8 +36,8 @@ $uvs_1d1u_site_id    =   'default'; // The site where you want to create the vou
 $uvs_1d1u_note       =   ''; // Note on the voucher
 $uvs_1d1u_expiration =   1440; // Expiration Time (1 Day = 1440)
 $uvs_1d1u_quota      =   1; // 1 = One time, 2 = Multi use
-$uvs_1d1u_up         =   5000; // Bandwidth Limit Upload in kbits
-$uvs_1d1u_down       =   2000; // Bandwidth Limit Download in kbits
+$uvs_1d1u_up         =   2000; // Bandwidth Limit Upload in kbits
+$uvs_1d1u_down       =   5000; // Bandwidth Limit Download in kbits
 $uvs_1d1u_limit      =   null; // Byte Quota per use in MB (null=unlimited)
 $uvs_1d1u_count      =   1; // How many vouchers - WARNING: Currently we can send just 1 voucher to the printer, so keep it for now.
 
@@ -46,8 +46,8 @@ $uvs_1d2u_site_id    =   'default'; // The site where you want to create the vou
 $uvs_1d2u_note       =   ''; // Note on the voucher
 $uvs_1d2u_expiration =   1440; // Expiration Time (1 Day = 1440)
 $uvs_1d2u_quota      =   2; // 1 = One time, 2 = Multi use
-$uvs_1d2u_up         =   5000; // Bandwidth Limit Upload in kbits
-$uvs_1d2u_down       =   2000; // Bandwidth Limit Download in kbits
+$uvs_1d2u_up         =   2000; // Bandwidth Limit Upload in kbits
+$uvs_1d2u_down       =   5000; // Bandwidth Limit Download in kbits
 $uvs_1d2u_limit      =   null; // Byte Quota per use in MB (null=unlimited)
 $uvs_1d2u_count      =   1; // How many vouchers - WARNING: Currently we can send just 1 voucher to the printer, so keep it for now.
 
@@ -56,8 +56,8 @@ $uvs_7d1u_site_id    =   'default'; // The site where you want to create the vou
 $uvs_7d1u_note       =   ''; // Note on the voucher
 $uvs_7d1u_expiration =   10080; // Expiration Time (1 Week = 10080)
 $uvs_7d1u_quota      =   1; // 1 = One time, 2 = Multi use
-$uvs_7d1u_up         =   5000; // Bandwidth Limit Upload in kbits
-$uvs_7d1u_down       =   2000; // Bandwidth Limit Download in kbits
+$uvs_7d1u_up         =   2000; // Bandwidth Limit Upload in kbits
+$uvs_7d1u_down       =   5000; // Bandwidth Limit Download in kbits
 $uvs_7d1u_limit      =   null; // Byte Quota per use in MB (null=unlimited)
 $uvs_7d1u_count      =   1; // How many vouchers - WARNING: Currently we can send just 1 voucher to the printer, so keep it for now.
 
@@ -66,8 +66,8 @@ $uvs_7d2u_site_id    =   'default'; // The site where you want to create the vou
 $uvs_7d2u_note       =   ''; // Note on the voucher
 $uvs_7d2u_expiration =   10080; // Expiration Time (1 Week = 10080)
 $uvs_7d2u_quota      =   2; // 1 = One time, 2 = Multi use
-$uvs_7d2u_up         =   5000; // Bandwidth Limit Upload in kbits
-$uvs_7d2u_down       =   2000; // Bandwidth Limit Download in kbits
+$uvs_7d2u_up         =   2000; // Bandwidth Limit Upload in kbits
+$uvs_7d2u_down       =   5000; // Bandwidth Limit Download in kbits
 $uvs_7d2u_limit      =   null; // Byte Quota per use in MB (null=unlimited)
 $uvs_7d2u_count      =   1; // How many vouchers - WARNING: Currently we can send just 1 voucher to the printer, so keep it for now.
 
@@ -76,8 +76,8 @@ $uvs_31d1u_site_id    =   'default'; // The site where you want to create the vo
 $uvs_31d1u_note       =   ''; // Note on the voucher
 $uvs_31d1u_expiration =   44640; // Expiration Time (1 Month = 44640)
 $uvs_31d1u_quota      =   1; // 1 = One time, 2 = Multi use
-$uvs_31d1u_up         =   10000; // Bandwidth Limit Upload in kbits
-$uvs_31d1u_down       =   5000; // Bandwidth Limit Download in kbits
+$uvs_31d1u_up         =   5000; // Bandwidth Limit Upload in kbits
+$uvs_31d1u_down       =   10000; // Bandwidth Limit Download in kbits
 $uvs_31d1u_limit      =   null; // Byte Quota per use in MB (null=unlimited)
 $uvs_31d1u_count      =   1; // How many vouchers - WARNING: Currently we can send just 1 voucher to the printer, so keep it for now.
 
@@ -86,8 +86,8 @@ $uvs_31d2u_site_id    =   'default'; // The site where you want to create the vo
 $uvs_31d2u_note       =   ''; // Note on the voucher
 $uvs_31d2u_expiration =   44640; // Expiration Time (1 Month = 44640)
 $uvs_31d2u_quota      =   2; // 1 = One time, 2 = Multi use
-$uvs_31d2u_up         =   4000; // Bandwidth Limit Upload in kbits
-$uvs_31d2u_down       =   28800; // Bandwidth Limit Download in kbits
+$uvs_31d2u_up         =   5000; // Bandwidth Limit Upload in kbits
+$uvs_31d2u_down       =   10000; // Bandwidth Limit Download in kbits
 $uvs_31d2u_limit      =   null; // Byte Quota per use in MB (null=unlimited)
 $uvs_31d2u_count      =   1; // How many vouchers - WARNING: Currently we can send just 1 voucher to the printer, so keep it for now.
 
